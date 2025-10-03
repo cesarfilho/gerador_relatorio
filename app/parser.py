c85d2945c87d47a2cfc6760e0fda78e278c02f91
Cesar Roberto Pamplona Filho <cesarpamplonafilho@gmail.com>
2025-10-03 14:20:27 -0300

    correcoes testes

--- app/parser.py
+++ app/parser.py
@@ -19,6 +19,7 @@ class ArgumentoCli:
         self.read_csv = reader_csv
         self.export = export
         self.definir_argumentos()
+        self.processar()
 
     def definir_argumentos(self):
         self.parser.add_argument("filename", type=str, help="nome e caminho do arquivo")
@@ -27,14 +28,13 @@ class ArgumentoCli:
         self.parser.add_argument("-f","--format", choices=["json", "text"], help="Tipo de saída (json ou text)")
 
     def processar(self, args=None):
-        parsed = self.parser.parse_args(args)
+        parsed,unknown = self.parser.parse_known_args()
         formato = getattr(parsed, "format", None)
         start = getattr(parsed, "start", None)
         end = getattr(parsed, "end", None)
         filename = getattr(parsed, "filename", None)
         if not filename:
-            self.parser.print_help()
-            sys.exit(1)
+            return None
             
         dados = self.read_csv.read_csv(filename)
         logger.info(f"Lendo arquivo: {filename} com {len(dados)} linhas")
@@ -44,28 +44,6 @@ class ArgumentoCli:
             end=end,
             format=formato
         )
-
-    def processar(self):
-        if "format" in self.parser.parse_args():
-            formato = self.parser.parse_args().format
-        
-        if "start" in self.parser.parse_args():
-            start = self.parser.parse_args().start
-
-        if "end" in self.parser.parse_args():
-            end = self.parser.parse_args().end  
-
-        filename = self.parser.parse_args().filename
-
-        dados = self.read_csv.read_csv(filename)
-        logger.info(f"Lendo arquivo: {filename} com {len(dados)} linhas")
-
-        self.relatorio.parametros(
-            data=dados,
-            start=start,
-            end=end,
-            format=formato
-        )
         self.relatorio.gerar()
-        
+            
         self.export.exportar(self.relatorio.dados, formato)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,62 +1,69 @@
+import sys
 import unittest
-# from unittest.mock import patch, Mock
+from unittest.mock import patch
 
 from app.run import main
 from app.parser import ArgumentoCli
+from app.core import Relatorio
+from app.file import ReadCSV
+from app.output import Export
 
 class TestApp(unittest.TestCase):
+    def setUp(self):
+        self.rel=Relatorio()
+        self.reader=ReadCSV()
+        self.export=Export()
 
+    @patch.object(sys, 'argv', ['vendas-cli',"vendas_com_erros_v2.csv", "--format", "text"])
     def test_arg_format_text(self):
-        pargs = ["teste.csv", "--format", "text"]
-        p = ArgumentoCli()
-        _args = p.parser.parse_args(pargs)
-        self.assertEqual(_args.filename, "teste.csv")
+        self.parser = ArgumentoCli(relatorio=self.rel, reader_csv=self.reader, export=self.export)
+        _args = self.parser.parser.parse_args()
+        self.assertEqual(_args.filename, "vendas_com_erros_v2.csv")
         self.assertEqual(_args.format, "text")
 
+    @patch.object(sys, 'argv', ['vendas-cli',"vendas_com_erros_v2.csv", "--format", "json"])
     def test_arg_format_json(self):
-        pargs = ["teste.csv", "--format", "json"]
-        p = ArgumentoCli()
-        _args = p.parser.parse_args(pargs)
-        self.assertEqual(_args.filename, "teste.csv")
+        self.parser = ArgumentoCli(relatorio=self.rel, reader_csv=self.reader, export=self.export)
+        _args = self.parser.parser.parse_args()
+        self.assertEqual(_args.filename, "vendas_com_erros_v2.csv")
         self.assertEqual(_args.format, "json")
 
+    @patch.object(sys, 'argv', ['vendas-cli',"vendas_com_erros_v2.csv", "--format", "json","--start", "2023-01-01"])
     def test_arg_data_inicio(self):
-        pargs = ["teste.csv", "--start", "2023-01-01"]
-        p = ArgumentoCli()
-        _args = p.parser.parse_args(pargs)
+        self.parser = ArgumentoCli(relatorio=self.rel, reader_csv=self.reader, export=self.export)
+        _args = self.parser.parser.parse_args()
         self.assertEqual(_args.start, "2023-01-01")
 
+    @patch.object(sys, 'argv', ['vendas-cli',"vendas_com_erros_v2.csv", "--format", "json","--end", "2023-12-31"])
     def test_arg_data_fim(self):
-        pargs = ["teste.csv", "--end", "2023-12-31"]
-        p = ArgumentoCli()
-        _args = p.parser.parse_args(pargs)
+        self.parser = ArgumentoCli(relatorio=self.rel, reader_csv=self.reader, export=self.export)
+        _args = self.parser.parser.parse_args()
         self.assertEqual(_args.end, "2023-12-31")
 
+    @patch.object(sys, 'argv', ['vendas-cli', "--format", "json"])
     def test_arg_missing_filename(self):
-        pargs = []
-        p = ArgumentoCli()
         with self.assertRaises(SystemExit):
-            p.parser.parse_args(pargs)
+            self.parser = ArgumentoCli(relatorio=self.rel, reader_csv=self.reader, export=self.export)
 
+    @patch.object(sys, 'argv', ['vendas-cli',"--help"])
     def test_arg_help(self):
-        pargs = ["--help"]
-        p = ArgumentoCli()
         with self.assertRaises(SystemExit):
-            p.parser.parse_args(pargs)
+            self.parser = ArgumentoCli(relatorio=self.rel, reader_csv=self.reader, export=self.export)
 
+    @patch.object(sys, 'argv', ['vendas-cli',"vendas_com_erros_v2.csv", "--format", "json"])
     def test_app_inicial(self):
         p = main()
         self.assertIsNotNone(p)
         self.assertIsInstance(p, ArgumentoCli)
 
+    @patch.object(sys, 'argv', ['vendas-cli',"vendas_com_erros_v2.csv","--start", "2023-01-01","--format", "json"])
     def test_arg_main(self):
-        pargs = ["teste.csv", "--start", "2023-01-01"]
-        p = main()
-        self.assertIsNotNone(p)
-        self.assertIsInstance(p, ArgumentoCli)
-        _args = p.parser.parse_args(pargs)
+        self.parser = main()
+        self.assertIsNotNone(self.parser)
+        self.assertIsInstance(self.parser, ArgumentoCli)
+        _args = self.parser.parser.parse_args()
         self.assertIsNotNone(_args)
-        self.assertEqual(_args.filename, "teste.csv")
+        self.assertEqual(_args.filename, "vendas_com_erros_v2.csv")
         self.assertEqual(_args.start, "2023-01-01")
         self.assertIsNone(_args.end)
-        self.assertIsNone(_args.format)
+        self.assertEqual(_args.format, "json")
